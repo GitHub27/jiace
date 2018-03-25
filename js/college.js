642259ee4874fce81a0076320f7a9df4350fe809
GitHub27 <wangjingli27@163.com>
2018-03-25 17:04:39 +0800

    '周日-2018年3月25日17:04:34'

---
+++ js/college.js
@@ -0,0 +1,32 @@
+$(function () {
+    $(".region-name").mouseover(function (e) {
+        var _width = $(e.target).offset().left
+        $(e.target).addClass('active').siblings("div").removeClass('active');
+        var width = 0;
+        var preAll = $(e.target).prevAll("div").each(function (i) {
+            width += $(this).innerWidth();
+        });
+        $(".region-cities").show().css('margin-left', (width + 167) + 'px')
+    })
+    var pageNumber = 1;
+    var pageSize = 12;
+
+    function pagination(records) {
+        $("#pagination").pagination(records, {
+            num_edge_entries: 1,
+            num_display_entries: 4,
+            current_page: pageNumber - 1,
+            items_per_page: pageSize,
+            prev_text: "上一页",
+            next_text: "下一页",
+            callback: page_index
+        });
+    }
+    pagination(100);
+
+    function page_index(page_index) {
+        console.log('页数', page_index);
+        var pageNumber = page_index + 1;
+        //queryNearbyCompany(pageNumber, pageSize, obj.more, obj.privince, obj.area);
+    };
+})
--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
 $(function () {
-    var pageNumber = 1;
-    var pageSize = 12;
-    function pagination(records) {
-        $("#pagination").pagination(records, {
-            num_edge_entries: 1,
-            num_display_entries: 4,
-            current_page: pageNumber - 1,
-            items_per_page: pageSize,
-            prev_text: "上一页",
-            next_text: "下一页",
-            callback: page_index
-        });
-    }
-    pagination(100);
-    function page_index(page_index) {
-        console.log('页数', page_index);
-        var pageNumber = page_index + 1;
-        //queryNearbyCompany(pageNumber, pageSize, obj.more, obj.privince, obj.area);
-    };
+
 })
