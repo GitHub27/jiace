797ec35f57863cf1b93bc440b857e3f86f41fa7c
wangjingli <wangjingli@zillionfortune.com>
2018-03-29 18:56:48 +0800

    完成部分

--- js/course-detail.js
+++ js/course-detail.js
@@ -65,33 +65,29 @@ $(function () {
     //学院列表
     function getCourseList(iscb) {
         $.AkmiiAjaxGet(window.api_list.schoollist_incourse, { "courseId": courseid, 'pageIndex': pageNumber, 'pageSize': pageSize }, false).then(function (d) {
-            if (d.jsonData) {
+            if (d.jsonData1) {
                 var str = '';
                 var template = '<div class="course-item"><div class="course-detail"><div class="course-column course-logo">\
-                <img src="/images/course-logo.png"></div><div class="course-column"><p class="course-name">家策健康学院</p><p class="course-info">\
-                <img src="/images/course-teacher.png">王老师、杨老师</p><p class="course-info"><img src="/images/course-phone.png">021-84188797</p>\
-                <p class="course-info"><img src="/images/course-address.png">上海市徐汇区南丹东路300号亚都商务楼1201室</p></div><div class="course-column score">\
-                <p><img src="/images/stars.png"></p><p class="score-text">母婴护理 育婴师 家政服务员 健康照护 养老护理员 泌乳师</p></div></div></div>';
+                <img src="{0}"></div><div class="course-column"><p class="course-name">{1}</p><p class="course-info">\
+                <img src="/images/course-teacher.png">{2}</p><p class="course-info"><img src="/images/course-phone.png">{3}</p>\
+                <p class="course-info"><img src="/images/course-address.png">{4}</p></div><div class="course-column score">\
+                <p><img src="/images/stars.png"></p><p class="score-text">{5}</p></div></div></div>';
                 d.jsonData.rows.forEach(function (item) {
                     str += template.format([
-                        d.jsonData.courseImg || "/images/course-detail-logo.png",
-                        gradeImg(d.jsonData.authenticateGrade),
-                        d.jsonData.courseName,
-                        d.jsonData.orderNum,
-                        d.jsonData.courseSuitableJob,
-                        d.jsonData.totalHours,
-                        d.jsonData.authenticateGrade,
-                        d.jsonData.authorityName,
+                        item.logoUrl || "/images/course-logo.png",
+                        item.schoolName,
+                        item.contacts,
+                        item.schoolPhone,
+                        item.schoolAddress,
+                        item.learnTypes
                     ])
                 });
-                $(".course-detail-head").replaceWith(str);
-                $("#outline .info-item").html(d.jsonData.courseBrief);//简介
-                $("#intro .info-item").html(d.jsonData.courseOutline);//大纲
+                $(".course-list").html(str)
                 if (!iscb) {
                     pagination(d.jsonData.records);
                 }
             } else {
-                $(".course-list").html('<div style="margin:300px 0 250px;background-color:#f6f6f6;" class="isnull" style="margin"><img src = "/images/null.png"></div>')
+                $(".course-list").html('<div style="margin:100px 0 50px;background-color:#fff;" class="isnull" style="margin"><img src = "/images/null.png"></div>')
             }
         }, function () {
 
